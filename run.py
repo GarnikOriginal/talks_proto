5dcf1e38ea2609370f78cc05505236945da3891c
GarnikOriginal <gavid19912@gmail.com>
2022-04-03 18:57:29 +0500

    Remove old comments

--- run.py
+++ run.py
@@ -18,27 +18,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    """
-    import av
-    import cv2
-    from time import sleep
-
-    camera = av.open('/dev/video0')
-    i = 0
-    cv2.namedWindow("1")
-    stream = camera.demux()
-
-    for packet in stream:
-        sleep(1e-2)
-        for frame in packet.decode():
-            cv2.imshow("1", frame.to_ndarray(format='bgr24'))
-            key = cv2.waitKey(1)
-            if key == 27 or i > 100:
-                break
-            i += 1
-            print(f"Frame: {i}")
-        if key == 27 or i > 100:
-            break
-    cv2.destroyAllWindows()
-    """
